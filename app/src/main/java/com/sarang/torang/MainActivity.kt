1ab0cba91486388e23a4ed854b522ddc0ebf82fd
sryang <sarang628@gmail.com>
2024-04-24 11:34:26 +0800

    update CommentBottomSheet

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -5,32 +5,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.ui.TorangTheme
 import com.sarang.torang.compose.comments.CommentsModal
@@ -54,24 +42,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             var showCommentDialog by remember { mutableStateOf(false) }
             val coroutine = rememberCoroutineScope()
-            val sheetValue by remember { mutableStateOf(SheetValue.Hidden) }
-            val sheetState: BottomSheetScaffoldState = rememberBottomSheetScaffoldState(
-                bottomSheetState = rememberStandardBottomSheetState(
-                    initialValue = sheetValue,
-                    skipHiddenState = false
-                )
-            )
-            var init by remember { mutableStateOf(true) }
+            var show by remember { mutableStateOf(false) }
             var reviewId: Int? by remember { mutableStateOf(329) }
-
-            LaunchedEffect(key1 = sheetState.bottomSheetState) {
-                snapshotFlow { sheetState.bottomSheetState.currentValue }.collect {
-                    if (it == SheetValue.Hidden) {
-                        Log.d("__sryang", "reviewId set null")
-                        reviewId = null
-                    }
-                }
-            }
 
 
             TorangTheme {
@@ -80,13 +52,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                     Column {
                         Button(onClick = {
                             coroutine.launch {
                                 reviewId = 329
-                                init = false
-                                sheetState.bottomSheetState.expand()
+                                show = true
                             }
                         }) {
                             Text(text = "showComment")
@@ -97,35 +67,25 @@ class MainActivity : ComponentActivity() {
                         }
                         LoginRepositoryTest(loginRepository = loginRepository)
                     }
-                    if (init == false && sheetState.bottomSheetState.currentValue != SheetValue.Hidden)
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .background(Color(0x55000000))
-                        ) {
-                            Text(text = "!!")
-                        }
 
-                    Box(modifier = Modifier) {
-                        CommentBottomSheet(
+
+                    CommentBottomSheet(
+                        reviewId = reviewId,
+                        onDismissRequest = {},
+                        onBackPressed = {},
+                        show = show,
+                        onHidden = {
+                            show = false
+                            reviewId = null
+                        },
+                        content = {}
+                    )
+
+                    if (showCommentDialog) {
+                        CommentsModal(
                             reviewId = reviewId,
-                            sheetState = sheetState,
-                            onDismissRequest = {},
-                            onBackPressed = {},
-                            init = init,
-                            onHidden = { init = true },
-                            content = {}
+                            onDismissRequest = { showCommentDialog = false },
                         )
-
-                        if (showCommentDialog) {
-                            CommentsModal(
-                                reviewId = reviewId,
-                                onDismissRequest = {
-                                    Log.d("__sryang", "reviewId = null")
-                                    showCommentDialog = false
-                                },
-                            )
-                        }
                     }
                 }
             }
